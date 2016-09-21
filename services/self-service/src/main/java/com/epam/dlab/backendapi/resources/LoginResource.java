94f891d83fd0bfd5872a87cea40a64e728ba04a4
Alexey_Suprun <Alexey_Suprun@epam.com>
2016-09-21 14:37:48 +0300

    [EPMCBDCCSS-518]: +refactoring

--- services/self-service/src/main/java/com/epam/dlab/backendapi/resources/LoginResource.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/resources/LoginResource.java
@@ -11,7 +11,6 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MediaType;
 
 
