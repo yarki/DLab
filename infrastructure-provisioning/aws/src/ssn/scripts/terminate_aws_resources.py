19d341ed8ca845c5be1f1b2a0d2083fe8e085e1d
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2016-09-26 14:43:15 +0300

    - remove duplicate functions (code review) e Please enter the commit message for your changes. Lines starting

--- infrastructure-provisioning/aws/src/ssn/scripts/terminate_aws_resources.py
+++ infrastructure-provisioning/aws/src/ssn/scripts/terminate_aws_resources.py
@@ -5,7 +5,6 @@
 import boto3
 import os
 import argparse
-from ConfigParser import SafeConfigParser
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dry_run', type=str, default='false')
@@ -18,73 +17,16 @@ parser.add_argument('--emr_name', type=str)
 args = parser.parse_args()
 
 
-# Function for parsing config files for parameters
-def get_configuration(configuration_dir):
-    merged_config = SafeConfigParser()
-
-    crid_config = SafeConfigParser()
-    crid_config.read(configuration_dir + 'aws_crids.ini')
-    for section in ['creds', 'ops']:
-        for option, value in crid_config.items(section):
-            if not merged_config.has_section(section):
-                merged_config.add_section(section)
-            merged_config.set(section, option, value)
-
-    base_infra_config = SafeConfigParser()
-    base_infra_config.read(configuration_dir + 'self_service_node.ini')
-    for section in ['conf', 'ssn']:
-        for option, value in base_infra_config.items(section):
-            if not merged_config.has_section(section):
-                merged_config.add_section(section)
-            merged_config.set(section, option, value)
-
-    notebook_config = SafeConfigParser()
-    notebook_config.read(configuration_dir + 'notebook.ini')
-    section = 'notebook'
-    for option, value in notebook_config.items(section):
-        if not merged_config.has_section(section):
-            merged_config.add_section(section)
-        merged_config.set(section, option, value)
-
-    overwrite_config = SafeConfigParser()
-    overwrite_config.read(configuration_dir + 'overwrite.ini')
-    for section in ['creds', 'conf', 'ssn', 'notebook']:
-        if overwrite_config.has_section(section):
-            if not merged_config.has_section(section):
-                merged_config.add_section(section)
-            for option, value in overwrite_config.items(section):
-                merged_config.set(section, option, value)
-
-    shadow_overwrite_config = SafeConfigParser()
-    shadow_overwrite_config.read(configuration_dir + 'shadow_overwrite.ini')
-    for section in ['creds', 'conf', 'ssn', 'notebook']:
-        if shadow_overwrite_config.has_section(section):
-            if not merged_config.has_section(section):
-                merged_config.add_section(section)
-            for option, value in shadow_overwrite_config.items(section):
-                merged_config.set(section, option, value)
-
-    return merged_config
-
-
-# Function for determining service_base_name from config
-def determine_service_base_name():
-    config = get_configuration(os.environ['PROVISION_CONFIG_DIR'])
-    service_base_name = config.get('conf', 'service_base_name')
-
-    return service_base_name
-
-
 # Function for terminating IAM roles
 def remove_role(notebook_name, instance_type):
     print "========= Role =========="
     client = boto3.client('iam')
     if instance_type == "ssn":
-        role_name = determine_service_base_name() + '-role'
-        role_profile_name = determine_service_base_name() + '-role-profile'
+        role_name = os.environ['conf_service_base_name'] + '-role'
+        role_profile_name = os.environ['conf_service_base_name'] + '-role-profile'
     elif instance_type == "notebook":
-        role_name = determine_service_base_name() + '-' + "{}".format(notebook_name) + '-role'
-        role_profile_name = determine_service_base_name() + '-' + "{}".format(notebook_name) + '-role-profile'
+        role_name = os.environ['conf_service_base_name'] + '-' + "{}".format(notebook_name) + '-role'
+        role_profile_name = os.environ['conf_service_base_name'] + '-' + "{}".format(notebook_name) + '-role-profile'
     try:
         role = client.get_role(RoleName="{}".format(role_name)).get("Role").get("RoleName")
     except:
@@ -122,8 +64,8 @@ def remove_ec2(ssn_tag_value, notebook_tag_value):
     print "========== EC2 =========="
     ec2 = boto3.resource('ec2')
     client = boto3.client('ec2')
-    ssn_tag_name = determine_service_base_name() + '-tag'
-    notebook_tag_name = determine_service_base_name()
+    ssn_tag_name = os.environ['conf_service_base_name'] + '-tag'
+    notebook_tag_name = os.environ['conf_service_base_name']
     ssn_instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped']},
                  {'Name': 'tag:{}'.format(ssn_tag_name), 'Values': ['{}'.format(ssn_tag_value)]}])
@@ -154,7 +96,7 @@ def remove_sgroups():
     print "========== SG ==========="
     ec2 = boto3.resource('ec2')
     client = boto3.client('ec2')
-    tag_name = determine_service_base_name() + '-tag'
+    tag_name = os.environ['conf_service_base_name'] + '-tag'
     sgs = ec2.security_groups.filter(
         Filters=[{'Name': 'tag:{}'.format(tag_name), 'Values': ['*']}])
     for sg in sgs:
@@ -168,7 +110,7 @@ def remove_subnets():
     print "======== Subnet ========="
     ec2 = boto3.resource('ec2')
     client = boto3.client('ec2')
-    tag_name = determine_service_base_name() + '-tag'
+    tag_name = os.environ['conf_service_base_name'] + '-tag'
     subnets = ec2.subnets.filter(
         Filters=[{'Name': 'tag:{}'.format(tag_name), 'Values': ['*']}])
     for subnet in subnets:
@@ -182,7 +124,7 @@ def remove_vpc():
     print "========== VPC =========="
     ec2 = boto3.resource('ec2')
     client = boto3.client('ec2')
-    tag_name = determine_service_base_name() + '-tag'
+    tag_name = os.environ['conf_service_base_name'] + '-tag'
     vpcs = ec2.vpcs.filter(
         Filters=[{'Name': 'tag:{}'.format(tag_name), 'Values': ['*']}])
     for vpc in vpcs:
@@ -196,7 +138,7 @@ def remove_s3():
     print "========== S3 ==========="
     s3 = boto3.resource('s3')
     client = boto3.client('s3')
-    bucket_name = (determine_service_base_name() + '-bucket').lower().replace('_', '-')
+    bucket_name = (os.environ['conf_service_base_name'] + '-bucket').lower().replace('_', '-')
     bucket = s3.Bucket("{}".format(bucket_name))
     print bucket.name
     list_obj = client.list_objects(Bucket=bucket.name)
