fa7ac07431baf027f8a9d17f7ffebdf3d3d64957
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-09-26 15:26:45 -0400

    refactoring to project structure recommendations

--- services/security-service/datalab-auth-ldap/src/main/java/com/epam/datalab/auth/ldap/LdapAuthenticationApp.java
+++ services/security-service/datalab-auth-ldap/src/main/java/com/epam/datalab/auth/ldap/LdapAuthenticationApp.java
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package com.epam.datalab.auth;
+package com.epam.datalab.auth.ldap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +21,8 @@ import org.slf4j.LoggerFactory;
 import com.epam.datalab.auth.DataLabAuthenticationConfig;
 import com.epam.datalab.auth.api.Login;
 import com.epam.datalab.auth.api.Logout;
+import com.epam.datalab.auth.ldap.api.Authenticate;
+import com.epam.datalab.auth.ldap.api.Authorize;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
