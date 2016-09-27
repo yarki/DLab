f802f46b9be35f05f64c32d561b05771d67a02b9
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-09-27 12:58:34 -0400

    add new structure

---
+++ services/security-service/auth-client-example/src/main/java/com/epam/dlab/auth/example/api/TestView.java
@@ -0,0 +1,52 @@
+/*
+Copyright 2016 EPAM Systems, Inc.
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.epam.dlab.auth.example.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.epam.dlab.auth.core.AuthenticationService;
+import com.epam.dlab.auth.core.UserInfo;
+
+import io.dropwizard.views.View;
+
+public class TestView extends View {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(TestView.class);
+	
+	private final UserInfo user ;
+	private final String accessToken;
+	private final AuthenticationService authenticationService;
+	
+	public TestView(UserInfo user, String token,AuthenticationService as) {
+		super("testauth.mustache");
+		this.user = user;
+		this.accessToken = token;
+		this.authenticationService = as;
+	}
+	
+	public String getName() {
+		return user.getName();
+	}
+	
+	public String getLogoutUrl() {
+		return authenticationService.getLogoutUrl();
+	}
+	
+	public String getAccessToken() {
+		return accessToken;
+	}
+}
