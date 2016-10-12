b1faaa4a3bbdc6708b0eb874d2e918a6e82a9643
Oleh_Hrynyk@epam.com <Oleh_Hrynyk@epam.com>
2016-10-12 13:01:23 +0300

    removed unused files

---
+++ services/self-service/src/main/resources/webapp/src/client/app/security/authorization.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserProfileService } from './userProfile.service';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  constructor(private userProfileService: UserProfileService, private router: Router) {}
+
+  canActivate() {
+    if(!this.userProfileService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    }
+    return  true;
+  }
+}
