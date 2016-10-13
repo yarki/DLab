bc55afa57a78c359caa63877c0bc96516b3896f1
mrsecret911 <stanislavmelnychek@gmail.com>
2016-10-13 18:05:43 +0300

    implemented upload key functionality

--- services/self-service/src/main/resources/webapp/src/client/app/home/home.component.ts
+++ services/self-service/src/main/resources/webapp/src/client/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
 import { AuthenticationService } from './../security/authentication.service';
-declare var $:any;
+import { UserProfileService } from "../security/userProfile.service";
 
 @Component({
   moduleId: module.id,
@@ -12,14 +14,53 @@ declare var $:any;
 })
 
 export class HomeComponent implements OnInit {
-  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+  key: any;
+  keyStatus: number;
+
+  constructor(private http: Http, private authenticationService: AuthenticationService, private router: Router, private userProfileService : UserProfileService) {}
 
    logout() {
      this.authenticationService.logout();
      this.router.navigate(['/login']);
    }
 
+
   ngOnInit() {
-     $('.upload_key').modal('show');
+    this.getMethod()
+    .subscribe(
+      data => {
+        this.key = data;
+      },
+      err => {
+        this.keyStatus = err.status;
+        this.openModal('.upload_key');
+      }
+    );
+  }
+
+  uploadKey(event) {
+    this.uploadPreloader();
+  }
+
+  uploadPreloader() {
+    this.openModal('.loading_modal');
+    setTimeout(function () {
+      this.closeModal('.loading_modal');
+    }.bind(this), 10000);
+  }
+
+  getMethod() {
+    return this.http.get(`/api/keyloader=?access_token=${this.userProfileService.getAuthToken()}`).map(( res:Response ) => res.json());
+  }
+
+  openModal(className: string) {
+    [].forEach.call(document.querySelectorAll('.modal'), function(modal) {
+      modal.setAttribute('open', '');
+    });
+    document.querySelector(className).setAttribute('open', 'true');
+  }
+
+  closeModal(className: string) {
+    document.querySelector(className).setAttribute('open', '');
   }
 }
--- services/self-service/src/main/resources/webapp/src/client/app/security/userProfile.service.ts
+++ services/self-service/src/main/resources/webapp/src/client/app/security/userProfile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 import { WebRequestHelper } from './../util/webRequestHelper.service'
 
 @Injectable()
