156e52e82dd8c03d41678c035e17db3fa5565fa3
Alexey_Suprun <Alexey_Suprun@epam.com>
2016-10-18 16:43:24 +0300

    fix rest names

--- services/provisioning-service/src/main/java/com/epam/dlab/backendapi/client/rest/SelfAPI.java
+++ services/provisioning-service/src/main/java/com/epam/dlab/backendapi/client/rest/SelfAPI.java
@@ -4,5 +4,5 @@ package com.epam.dlab.backendapi.client.rest;
  * Created by Alexey Suprun
  */
 public interface SelfAPI {
-    String KEY_LOADER = "api/keyloader";
+    String KEY_LOADER = "api/keyloader/callback";
 }
