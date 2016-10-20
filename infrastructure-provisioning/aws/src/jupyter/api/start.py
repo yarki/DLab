b659cdc1e5a7cf5c34331879f790ee39844991d2
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2016-10-20 16:21:58 +0300

    [EPMCBDCCSS-648] - add start functionality

--- infrastructure-provisioning/aws/src/jupyter/api/start.py
+++ infrastructure-provisioning/aws/src/jupyter/api/start.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python
-from ConfigParser import SafeConfigParser
 import os
-from fabric.api import local, hide
+import json
+import sys
+from fabric.api import local
+
 
 if __name__ == "__main__":
-#    with hide('stderr', 'running'):
-#        local("cd /root; fab run")
-    print '{"message": "justatest"}'
+    success = True
+    try:
+        local('cd /root; fab start')
+    except:
+        success = False
+
+    reply = dict()
+    reply['request_id'] = os.environ['request_id']
+    if success:
+        reply['status'] = 'ok'
+    else:
+        reply['status'] = 'err'
+
+    reply['response'] = dict()
+
+    try:
+        with open("/root/result.json") as f:
+            reply['response']['result'] = json.loads(f.read())
+    except:
+        reply['response']['result'] = {"error": "Failed to open result itself. Bad sign."}
+        pass
+
+    reply['response']['log'] = "/response/%s.log" % os.environ['request_id']
+
+    with open("/response/%s.json" % os.environ['request_id'], 'w') as response_file:
+        response_file.write(json.dumps(reply))
+
+    if not success:
+        sys.exit(1)
