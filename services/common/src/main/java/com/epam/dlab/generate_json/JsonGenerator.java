6c320f0cf6e22af35bb252cac6ced749eeab1b34
Maksym Pendyshchuk <maksym_pendyshchuk@epam.com>
2016-10-21 22:21:00 +0300

    cleand up the code

--- services/common/src/main/java/com/epam/dlab/generate_json/JsonGenerator.java
+++ services/common/src/main/java/com/epam/dlab/generate_json/JsonGenerator.java
@@ -1,9 +1,5 @@
 package com.epam.dlab.generate_json;
 
-//import com.epam.dlab.generate_json.docker.run.parameters.CreateEMRClusterParameters;
-//import com.epam.dlab.generate_json.docker.run.parameters.DockerRunParameters;
-import com.epam.dlab.dto.EMRBaseDTO;
-import com.epam.dlab.dto.EMRCreateDTO;
 import com.epam.dlab.dto.ResourceBaseDTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,39 +25,4 @@ public class JsonGenerator {
         }
     }
 
-    /*public static void main(String[] args) throws JsonProcessingException {
-        System.out.println(
-                new JsonGenerator().generateJson(
-                        new EMRCreateDTO().withInstanceCount("2")
-                        ,
-                        true
-                )
-        );
-    }*/
-
-    /*public String generateJson(DockerRunParameters parameters) throws JsonProcessingException {
-        return generateJson(parameters, false);
-    }
-
-    public String generateJson(DockerRunParameters parameters, boolean pretty) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        if(pretty) {
-            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(parameters);
-        }
-        else {
-            return objectMapper.writeValueAsString(parameters);
-        }
-    }
-
-    public static void main(String[] args) throws JsonProcessingException {
-        System.out.println(
-                new JsonGenerator().generateJson(
-                        CreateEMRClusterParameters.newCreateEMRClusterParameters()
-                        .confServiceBaseName("someName")
-                        .emrTimeout("10")
-                        .build(),
-                        true
-                )
-        );
-    }*/
 }
