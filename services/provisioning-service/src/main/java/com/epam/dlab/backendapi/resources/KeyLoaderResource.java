53e5dbc9e00cb4f4aa45d4e80780d4925b6f46a2
Alexey_Suprun <Alexey_Suprun@epam.com>
2016-10-25 17:04:14 +0300

    [EPMCBDCCSS-684]: delete key when exception occured

--- services/provisioning-service/src/main/java/com/epam/dlab/backendapi/resources/KeyLoaderResource.java
+++ services/provisioning-service/src/main/java/com/epam/dlab/backendapi/resources/KeyLoaderResource.java
@@ -23,6 +23,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 @Path("/keyloader")
@@ -35,8 +36,9 @@ public class KeyLoaderResource {
     private KeyLoader keyLoader;
 
     @POST
-    public String loadKey(UploadFileDTO dto) throws IOException, InterruptedException {
+    public Response loadKey(UploadFileDTO dto) throws IOException, InterruptedException {
         LOGGER.debug("load key for user {}", dto.getUser());
-        return keyLoader.uploadKey(dto);
+        keyLoader.uploadKey(dto);
+        return Response.accepted().build();
     }
 }
