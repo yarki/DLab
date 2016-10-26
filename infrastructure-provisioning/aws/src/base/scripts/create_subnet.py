7c3978e9c5eb05096230d52f4a1c31f965504e56
volodymyr_shvachko <volodymyr_shvachko@epam.com>
2016-10-26 11:49:03 +0300

    Fixed typo

--- infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
+++ infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
@@ -31,49 +31,45 @@ args = parser.parse_args()
 if __name__ == "__main__":
     success = False
     tag = {"Key": args.infra_tag_name, "Value": args.infra_tag_value}
-    if args.subnet != '':
-        try:
-            ec2 = boto3.resource('ec2')
-            vpc = ec2.Vpc(args.vpc_id)
-            cidr = vpc.cidr_block
-            subnets = list(vpc.subnets.all())
+    try:
+        ec2 = boto3.resource('ec2')
+        vpc = ec2.Vpc(args.vpc_id)
+        cidr = vpc.cidr_block
+        subnets = list(vpc.subnets.all())
 
-            cidr_blocks = []
-            for i in subnets:
-                cidr_blocks.append({'Net': i.cidr_block.split('/')[0], 'Mask': i.cidr_block.split('/')[1], 'NTOA': socket.inet_aton(i.cidr_block.split('/')[0])})
-            list = sorted(cidr_blocks, key=lambda k: k['NTOA'])
+        cidr_blocks = []
+        for i in subnets:
+            cidr_blocks.append({'Net': i.cidr_block.split('/')[0], 'Mask': i.cidr_block.split('/')[1], 'NTOA': socket.inet_aton(i.cidr_block.split('/')[0])})
+        list = sorted(cidr_blocks, key=lambda k: k['NTOA'])
 
-            position = 0
-            for i in list:
-                start = int(i.get('Net').split('.')[2])
-                #print 'start: ' + str(start)
-                end = start + ((lambda k: 2**(32 - k))(int(i.get('Mask')))) // 256
-                #print 'end: ' + str(end)
-                if position != start:
-                    #return position += 1
-                    position += 1
-                    #print 'position: ' + str(position)
-                    break
-                else:
-                    position = end
+        position = 0
+        for i in list:
+            start = int(i.get('Net').split('.')[2])
+            #print 'start: ' + str(start)
+            end = start + ((lambda k: 2**(32 - k))(int(i.get('Mask')))) // 256
+            #print 'end: ' + str(end)
+            if position != start:
+                #return position += 1
+                #position += 1
                 #print 'position: ' + str(position)
-
-            subnet_cidr = '{}.{}.{}.0/24'.format(cidr.split('.')[0], cidr.split('.')[1], position)
-            os.environ['creds_private_subnet_cidr'] = subnet_cidr
-            subnet_id = get_subnet_by_cidr(subnet_cidr)
-            if subnet_id == '':
-                print "Creating subnet %s in vpc %s with tag %s." % \
-                      (subnet_cidr, args.vpc_id, json.dumps(tag))
-                subnet_id = create_subnet(args.vpc_id, args.subnet, tag)
+                break
             else:
-                print "REQUESTED SUBNET ALREADY EXISTS"
-            print "SUBNET_ID " + subnet_id
-            success = True
-        except:
-            success = False
-    else:
-        parser.print_help()
-        sys.exit(2)
+                position = end
+            #print 'position: ' + str(position)
+
+        subnet_cidr = '{}.{}.{}.0/24'.format(cidr.split('.')[0], cidr.split('.')[1], position)
+        os.environ['creds_private_subnet_cidr'] = subnet_cidr
+        subnet_id = get_subnet_by_cidr(subnet_cidr)
+        if subnet_id == '':
+            print "Creating subnet %s in vpc %s with tag %s." % \
+                  (subnet_cidr, args.vpc_id, json.dumps(tag))
+            subnet_id = create_subnet(args.vpc_id, args.subnet, tag)
+        else:
+            print "REQUESTED SUBNET ALREADY EXISTS"
+        print "SUBNET_ID " + subnet_id
+        success = True
+    except:
+        success = False
 
     if success:
         sys.exit(0)
