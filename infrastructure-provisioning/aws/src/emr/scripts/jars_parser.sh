6002e13237908dbfd69d7f44e131d51b0fefc3e1
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-10-26 11:57:28 +0300

    changes

---
+++ infrastructure-provisioning/aws/src/emr/scripts/jars_parser.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# ******************************************************************************************************
+#
+# Copyright (c) 2016 EPAM Systems Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including # without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject # to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. # IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH # # THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# ****************************************************************************************************/
+
+BUCKET_NAME=$1
+EMR_VERSION=$2
+REGION=$3
+SPARK_DEF_PATH="/usr/lib/conf/spark-defaults.conf"
+SPARK_DEF_PATH_LINE1=`cat $SPARK_DEF_PATH | grep spark.driver.extraClassPath | awk '{print $2}' | tr ':' '\n' | sed 's|/\*||g' | sed 's|$|/\*|g' | tr '\n' ' '`
+SPARK_DEF_PATH_LINE2=`cat $SPARK_DEF_PATH | grep spark.driver.extraLibraryPath | awk '{print $2}' | tr ':' '\n' | sed 's|/\*||g' | sed 's|$|/\*|g' | tr '\n' ' '`
+/bin/tar -zcvf /tmp/jars.tar.gz --no-recursion --absolute-names $SPARK_DEF_PATH_LINE1 $SPARK_DEF_PATH_LINE2
+aws s3 cp /tmp/jars.tar.gz s3://$BUCKET_NAME/jars/$EMR_VERSION/ --endpoint-url https://s3-$REGION.amazonaws.com --region $REGION
