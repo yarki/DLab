b4f5d0021148636bd0644ea549bcf28784fd2f1c
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-10-26 23:07:47 +0300

    changes

--- infrastructure-provisioning/aws/src/emr/scripts/create_configs.py
+++ infrastructure-provisioning/aws/src/emr/scripts/create_configs.py
@@ -116,12 +116,17 @@ def spark_defaults(args):
     s3_client = boto3.client('s3')
     s3_client.download_file(args.bucket, 'spark-defaults.conf', '/tmp/spark-defaults-emr.conf')
     local('touch /tmp/spark-defaults-temporary.conf')
-    local('cat /tmp/spark-defaults-emr.conf | grep spark.driver.extraClassPath | tr " " ":" | sed "s|:||1" | sed "s|:||1" | sed "s|:||1" | tr ":" "\n" | sed "s|^|/opt/' + args.emr_version + '/jars|g" | tr "\n" ":" | sed "s|/opt/' + args.emr_version + '/jars||1" | sed "s/\(.*\)\:/\1 /" | sed "s|:|    |1" | sed "r|$|" | sed "s|$|:' + missed_jar_path + '|" | sed "s|\(.*\)\ |\1|" > /tmp/spark-defaults-temporary.conf')
+    local('cat /tmp/spark-defaults-emr.conf | grep spark.driver.extraClassPath | tr " " ":" | sed "s|:||1" | sed "s|:||1" | sed "s|:||1" | tr ":" "\n" | sed "s|^|/opt/EMRVERSION/jars|g" | tr "\n" ":" | sed "s|/opt/EMRVERSION/jars||1" | sed "s/\(.*\)\:/\1 /" | sed "s|:|    |1" | sed "r|$|" | sed "s|$|:MISSEDJAR|" | sed "s|\(.*\)\ |\1|" > /tmp/spark-defaults-temporary.conf')
     local('printf "\n"')
-    local('cat /tmp/spark-defaults-emr.conf | grep spark.driver.extraLibraryPath | tr " " ":" | sed "s|:||1" | sed "s|:||1" | sed "s|:||1" | tr ":" "\n" | sed "s|^|/opt/' + args.emr_version + '/jars|g" | tr "\n" ":" | sed "s|/opt/' + args.emr_version + '/jars||1" | sed "s/\(.*\)\:/\1 /" | sed "s|:|    |1" >> /tmp/spark-defaults-temporary.conf')
-    local('sudo mv /tmp/spark-defaults-temporary.conf ' + spark_def_path)
-    # spark_def_path = '/opt/' + args.emr_version + '/' + 'spark-' + args.spark_version + '-bin-hadoop' + hadoop_version + '/conf/spark-defaults.conf'
-    # template_file = "/tmp/spark-defaults_template.conf"
+    local('cat /tmp/spark-defaults-emr.conf | grep spark.driver.extraLibraryPath | tr " " ":" | sed "s|:||1" | sed "s|:||1" | sed "s|:||1" | tr ":" "\n" | sed "s|^|/opt/EMRVERSION/jars|g" | tr "\n" ":" | sed "s|/opt/EMRVERSION/jars||1" | sed "s/\(.*\)\:/\1 /" | sed "s|:|    |1" >> /tmp/spark-defaults-temporary.conf')
+    #local('sudo mv /tmp/spark-defaults-temporary.conf ' + spark_def_path)
+    template_file = "/tmp/spark-defaults-temporary.conf"
+    with open(template_file, 'r') as f:
+        text = f.read()
+    text = text.replace('EMRVERSION', args.emr_version)
+    text = text.replace('MISSEDJAR', missed_jar_path)
+    with open(spark_def_path, 'w') as f:
+        f.write(text)
     # with open(spark_def_path, 'w') as out:
     #     with open(template_file) as tpl:
     #         for line in tpl:
