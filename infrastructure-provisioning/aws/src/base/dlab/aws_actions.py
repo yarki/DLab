94813d8937d83e4b913530aa10a5465eb6f0d910
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-11-04 15:35:29 +0200

    changes

--- infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
+++ infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
@@ -35,6 +35,11 @@ def put_to_bucket(bucket_name, local_file, destination_file):
 
 
 def create_s3_bucket(bucket_name, tag, region):
+    local_log_filename = "%s.log" % os.environ['request_id']
+    local_log_filepath = "/response/" + local_log_filename
+    logging.basicConfig(format='%(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.DEBUG,
+                        filename=local_log_filepath)
     try:
         s3 = boto3.resource('s3')
         bucket = s3.create_bucket(Bucket=bucket_name,
