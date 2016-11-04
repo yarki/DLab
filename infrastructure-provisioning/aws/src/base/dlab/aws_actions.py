6e65be1c80819ba80f31b5f17140c978d702e57c
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-11-04 15:42:46 +0200

    changes

--- infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
+++ infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
@@ -18,6 +18,13 @@ import json
 from fabric.api import *
 import logging
 
+local_log_filename = "%s.log" % os.environ['request_id']
+local_log_filepath = "/response/" + local_log_filename
+logging.basicConfig(format='%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.DEBUG,
+                    filename=local_log_filepath)
+
+
 def put_to_bucket(bucket_name, local_file, destination_file):
     try:
         s3 = boto3.client('s3')
