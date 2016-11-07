124a817a7aa8f427750eb50f4e872a1581873145
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2016-11-07 19:00:55 +0200

    moved computational resource dialog

--- services/self-service/src/main/resources/webapp/src/client/app/models/computationalResourceApplicationTemplate.model.ts
+++ services/self-service/src/main/resources/webapp/src/client/app/models/computationalResourceApplicationTemplate.model.ts
@@ -1,12 +1,15 @@
 import  { ComputationalResourceApplication } from "./computationalResourceApplication.model";
+import { ResourceShapeModel } from "./resourceShape.model";
 
 export class ComputationalResourceApplicationTemplate {
   version: string;
   applications: Array<ComputationalResourceApplication>
-
-  constructor(jsonModel:any) {
+  shapes: Array<ResourceShapeModel>;
+  
+  constructor(jsonModel:any, shapes: Array<ResourceShapeModel>) {
     this.version = jsonModel.version;
     this.applications = [];
+    this.shapes = shapes;
 
     if(jsonModel.applications && jsonModel.applications.length > 0)
       for (let index = 0; index < jsonModel.applications.length; index++)
