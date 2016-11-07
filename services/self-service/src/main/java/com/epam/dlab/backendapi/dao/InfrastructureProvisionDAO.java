5442de3a0c3d78e1a7f9d0c56dd469e4d8dde95d
Alexey Suprun <Alexey_Suprun@epam.com>
2016-11-07 23:04:40 +0300

    [EPMCBDCCSS-797]: emr terminate

--- services/self-service/src/main/java/com/epam/dlab/backendapi/dao/InfrastructureProvisionDAO.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/dao/InfrastructureProvisionDAO.java
@@ -23,8 +23,10 @@ import com.mongodb.client.FindIterable;
 import org.bson.Document;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.elemMatch;
@@ -117,14 +119,13 @@ public class InfrastructureProvisionDAO extends BaseDAO {
 
     @SuppressWarnings("unchecked")
     public String fetchComputationalId(String user, String exploratoryName, String computationalName) {
-        Map<String, Object> resources = (Map) Optional.ofNullable(
-                mongoService.getCollection(USER_INSTANCES)
-                        .find(and(eq(USER, user), eq(EXPLORATORY_NAME, exploratoryName),
-                                eq(COMPUTATIONAL_RESOURCES + FIELD_DELIMETER + COMPUTATIONAL_NAME, computationalName)))
-                        .projection(elemMatch(COMPUTATIONAL_RESOURCES, eq(COMPUTATIONAL_NAME, computationalName))).first())
-                .orElse(new Document())
-                .getOrDefault(COMPUTATIONAL_RESOURCES, Collections.emptyMap());
-        return resources.getOrDefault(COMPUTATIONAL_ID, EMPTY).toString();
+        return find(USER_INSTANCES, and(eq(USER, user), eq(EXPLORATORY_NAME, exploratoryName)), UserInstanceDTO.class)
+                .flatMap(exploratory -> exploratory.getResources()
+                        .stream()
+                        .filter(computational -> computationalName.equals(computational.getComputationalName()))
+                        .findFirst())
+                .flatMap(computational -> Optional.ofNullable(computational.getComputationalId()))
+                .orElse(EMPTY);
     }
 
     public void updateComputationalStatus(ComputationalStatusDTO dto) {
