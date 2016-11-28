69ad542b0f4c6ee4d3e8af18de536cfa2227a28f
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2016-11-28 14:09:35 +0200

    [EPMCBDCCSS-910] -add basic functionality for rstudio template

---
+++ infrastructure-provisioning/aws/src/rstudio/scripts/configure_rstudio.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+# *****************************************************************************
+#
+# Copyright (c) 2016, EPAM SYSTEMS INC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ******************************************************************************
+
+from fabric.api import *
+from fabric.contrib.files import exists
+import argparse
+import json
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hostname', type=str, default='')
+parser.add_argument('--keyfile', type=str, default='')
+args = parser.parse_args()
+
+
+def prepare_disk():
+    if not exists('/home/ubuntu/.ensure_dir/disk_ensured'):
+        try:
+            sudo('''bash -c 'echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdb' ''')
+            sudo('mkfs.ext4 /dev/xvdb1')
+            sudo('mount /dev/xvdb1 /opt/')
+            sudo(''' bash -c "echo '/dev/xvdb1 /opt/ ext4 errors=remount-ro 0 1' >> /etc/fstab" ''')
+            sudo('touch /home/ubuntu/.ensure_dir/disk_ensured')
+        except:
+            sys.exit(1)
+
+
+def install_rstudio():
+    if not exists('/home/ubuntu/.ensure_dir/rstudio_ensured'):
+        try:
+            sudo('apt-get install -y r-base')
+            sudo('apt-get install -y gdebi-core')
+            sudo('wget https://download2.rstudio.org/rstudio-server-1.0.44-amd64.deb')
+            sudo('gdebi -n rstudio-server-1.0.44-amd64.deb')
+        except:
+            sys.exit(1)
+
+
+##############
+# Run script #
+##############
+if __name__ == "__main__":
+    print "Configure connections"
+    env['connection_attempts'] = 100
+    env.key_filename = [args.keyfile]
+    env.host_string = 'ubuntu@' + args.hostname
+    deeper_config = json.loads(args.additional_config)
+
+    print "Configuring notebook server."
+    try:
+        if not exists('/home/ubuntu/.ensure_dir'):
+            sudo('mkdir /home/ubuntu/.ensure_dir')
+    except:
+        sys.exit(1)
+    prepare_disk()
+    install_rstudio()
