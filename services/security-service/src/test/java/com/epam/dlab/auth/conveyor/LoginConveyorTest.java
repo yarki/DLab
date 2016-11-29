d2052bd16dc89824448772f590e5629107eda0bc
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-11-29 09:27:42 -0500

    fixed unit test

--- services/security-service/src/test/java/com/epam/dlab/auth/conveyor/LoginConveyorTest.java
+++ services/security-service/src/test/java/com/epam/dlab/auth/conveyor/LoginConveyorTest.java
@@ -1,14 +1,13 @@
 package com.epam.dlab.auth.conveyor;
 
+import com.amazonaws.services.identitymanagement.model.AccessKeyMetadata;
 import com.epam.dlab.auth.UserInfo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.concurrent.*;
 import java.util.function.Supplier;
 
 /**
@@ -42,36 +41,38 @@ public class LoginConveyorTest {
         lc.add("1","127.0.0.1",LoginStep.REMOTE_IP);
         lc.add("1",uiSource,LoginStep.LDAP_USER_INFO);
         lc.add("1",true,LoginStep.AWS_USER);
+        lc.add("1",new ArrayList<AccessKeyMetadata>() ,LoginStep.AWS_KEYS);
 
         UserInfo ui = uf.get(5, TimeUnit.SECONDS);
         System.out.println("Future now: "+ui);
     }
 
     @Test(expected = CancellationException.class)
-    public void cacheTest() throws ExecutionException, InterruptedException {
-        LoginCache lc = LoginCache.getInstance();
-
+    public void cacheTest() throws ExecutionException, InterruptedException, TimeoutException {
+        LoginCache cache = LoginCache.getInstance();
+System.out.println("---cacheTest");
         //Just for this test
-        lc.setDefaultBuilderTimeout(1,TimeUnit.SECONDS);
-        lc.setExpirationPostponeTime(1,TimeUnit.SECONDS);
+        cache.setDefaultBuilderTimeout(1,TimeUnit.SECONDS);
+        cache.setExpirationPostponeTime(1,TimeUnit.SECONDS);
 
         UserInfo userInfo = new UserInfo("test","user");
         userInfo.setFirstName("Mike");
         userInfo.setLastName("T");
         userInfo.addRole("tr");
         userInfo.setAwsUser(true);
+        userInfo.addKey("a","Active");
 
-        CompletableFuture<Boolean> f = lc.add("1",userInfo,LoginStep.USER_INFO);
-        CompletableFuture<UserInfo> uif = lc.getFuture("1");
+        CompletableFuture<Boolean> f = cache.add("2",userInfo,LoginStep.USER_INFO);
+        CompletableFuture<UserInfo> uif = cache.getFuture("2");
         f.get();
         //this will take at least 2 seconds
         for(int i = 0; i < 10; i++) {
-            UserInfo ui = lc.getUserInfo("1");
+            UserInfo ui = cache.getUserInfo("1");
             System.out.println(i+": "+ui);
             Thread.sleep(200);
         }
         //and finally will exit with timeout
-        uif.get();
+        uif.get(5,TimeUnit.SECONDS);
     }
 
     @Test
