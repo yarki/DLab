5d5aa27890d80d7c40df7ba172757fa2d033ad0c
Vadym_Kuznetsov <Vadym_Kuznetsov@epam.com>
2016-12-09 16:02:22 +0200

    - changes for debug

--- infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
+++ infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
@@ -376,9 +376,11 @@ def remove_s3(bucket_type='all', scientist=''):
         else:
             bucket_name = (os.environ['conf_service_base_name'])
         for item in client.list_buckets().get('Buckets'):
-            if bucket_name in str(item.get('Name')):
+            if bucket_name in item.get('Name'):
                 print item.get('Name')
                 bucket_list.append(item.get('Name'))
+            else:
+                print item.get('Name')
         print bucket_list
         for s3bucket in bucket_list:
             list_obj = client.list_objects(Bucket=s3bucket)
