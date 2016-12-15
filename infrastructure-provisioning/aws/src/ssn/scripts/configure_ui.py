6274c56152213501036f0fe3e858bc94e5ab20fc
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-12-15 15:31:20 +0200

    fixed MongoDB starting

--- infrastructure-provisioning/aws/src/ssn/scripts/configure_ui.py
+++ infrastructure-provisioning/aws/src/ssn/scripts/configure_ui.py
@@ -59,7 +59,8 @@ def ensure_mongo():
             sudo('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927')
             sudo('ver=`lsb_release -cs`; echo "deb http://repo.mongodb.org/apt/ubuntu $ver/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list; apt-get update')
             sudo('apt-get -y install mongodb-org')
-            sudo('sysv-rc-conf mongod on')
+            sudo('update-rc.d mongod defaults')
+            sudo('update-rc.d mongod enable')
             sudo('touch /opt/dlab/tmp/mongo_ensured')
         return True
     except:
