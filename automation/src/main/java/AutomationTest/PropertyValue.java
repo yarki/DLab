852da9559d0559f3c2cbfa8cc172b1ca13949e98
Mariia_Rusyn <Mariia_Rusyn@epam.com>
2016-12-23 17:15:42 +0200

    Write data from property file

--- automation/src/main/java/AutomationTest/PropertyValue.java
+++ automation/src/main/java/AutomationTest/PropertyValue.java
@@ -23,6 +23,32 @@ public class PropertyValue {
                 pr.load(fin);
                                
                 result = pr.getProperty(propName);
+                
+                  String JENKINS_USERNANE=pr.getProperty("JENKINS_USERNANE");
+                  String  JENKINS_PASSWORD=pr.getProperty("JENKINS_PASSWORD");
+                  String  USERNANE=pr.getProperty("USERNANE");
+                  String  PASSWORD=pr.getProperty("PASSWORD");
+                  String  NOT_IAM_USERNAME=pr.getProperty("NOT_IAM_USERNAME");
+                  String  NOT_IAM_PASSWORD=pr.getProperty("NOT_IAM_PASSWORD");
+                  String  NOT_DLAB_USERNAME=pr.getProperty("NOT_DLAB_USERNAME");
+                  String  NOT_DLAB_PASSWORD=pr.getProperty("NOT_DLAB_PASSWORD");
+                  String JENKINS_JOB_URL=pr.getProperty("JENKINS_JOB_URL");
+                  String  USER_FOR_ACTIVATE_KEY=pr.getProperty("USER_FOR_ACTIVATE_KEY");
+                  String  PASSWORD_FOR_ACTIVATE_KEY=pr.getProperty("PASSWORD_FOR_ACTIVATE_KEY") ;
+                  
+                  System.out.println("JENKINS_USERNANE is " + JENKINS_USERNANE);
+                  System.out.println("JENKINS_PASSWORD is " + JENKINS_PASSWORD);
+                  System.out.println("USERNANE is " + USERNANE);
+                  System.out.println("PASSWORD is " + PASSWORD);
+                  System.out.println("NOT_IAM_USERNAME is " + NOT_IAM_USERNAME);
+                  System.out.println("NOT_IAM_PASSWORD is " + NOT_IAM_PASSWORD);
+                  System.out.println("NOT_DLAB_USERNAME is " + NOT_DLAB_USERNAME);
+                  System.out.println("NOT_DLAB_PASSWORD is " + NOT_DLAB_PASSWORD);
+                  System.out.println("JENKINS_JOB_URL is " + JENKINS_JOB_URL);
+                  System.out.println("USER_FOR_ACTIVATE_KEY is " + USER_FOR_ACTIVATE_KEY);
+                  System.out.println("PASSWORD_FOR_ACTIVATE_KEY is " + PASSWORD_FOR_ACTIVATE_KEY);
+                 
+                
                 if (result == null) throw new Exception ("Value from property file is equal null");
                  
         
