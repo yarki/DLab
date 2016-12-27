f80de2808790523993be092c40b40bf5e77855a5
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-12-27 12:58:44 +0200

    added sleep

--- automation/src/test/java/AutomationTest/TestServices.java
+++ automation/src/test/java/AutomationTest/TestServices.java
@@ -138,6 +138,7 @@ public class TestServices {
         
         System.out.println("2. Check login");
         System.out.println("3. Check validation");
+        int timeout = PropertyValue.get(PropertyValue.TEST_BEFORE_SLEEP_SECONDS, 0);
         Thread.sleep(timeout * 1000);
         LoginDto notIAMUserRequestBody = new LoginDto(PropertyValue.get(PropertyValue.NOT_IAM_USERNAME), PropertyValue.get(PropertyValue.NOT_IAM_PASSWORD), "");
         Response responseNotIAMUser = new HttpRequest().webApiPost(Path.LOGIN, ContentType.JSON, notIAMUserRequestBody);
