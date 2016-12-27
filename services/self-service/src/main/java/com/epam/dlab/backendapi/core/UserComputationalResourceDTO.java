3ff3b285189c0bea58ef06a5a555c84ff09681bf
Usein Faradzhev <Usein_Faradzhev@epam.com>
2016-12-27 16:28:32 +0400

    Comments a few packages

--- services/self-service/src/main/java/com/epam/dlab/backendapi/core/UserComputationalResourceDTO.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/core/UserComputationalResourceDTO.java
@@ -22,6 +22,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 
+/** Stores info about the user's computational resources for notebook.
+ */
 public class UserComputationalResourceDTO {
     @JsonProperty("computational_name")
     private String computationalName;
@@ -38,92 +40,113 @@ public class UserComputationalResourceDTO {
     @JsonProperty("total_instance_number")
     private String slaveNumber;
 
+    /** Returns name of computational resource. */
     public String getComputationalName() {
         return computationalName;
     }
 
+    /** Sets name of computational resource. */
     public void setComputationalName(String computationalName) {
         this.computationalName = computationalName;
     }
 
+    /** Sets name of computational resource. */
     public UserComputationalResourceDTO withComputationalName(String computationalName) {
         setComputationalName(computationalName);
         return this;
     }
 
+    /** Returns a unique id of computational resource. */
     public String getComputationalId() {
         return computationalId;
     }
 
+    /** Sets a unique id of computational resource. */
     public void setComputationalId(String computationalId) {
         this.computationalId = computationalId;
     }
 
+    /** Sets a unique id of computational resource. */
     public UserComputationalResourceDTO withComputationalId(String computationalId) {
         setComputationalId(computationalId);
         return this;
     }
 
+    /** Returns the status. */
     public String getStatus() {
         return status;
     }
 
+    /** Sets the status. */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /** Sets the status. */
     public UserComputationalResourceDTO withStatus(String status) {
         setStatus(status);
         return this;
     }
 
+    /** Returns the date and time when the resource has created. */
     public Date getUptime() {
         return uptime;
     }
 
+    /** Sets the date and time when the resource has created. */
     public void setUptime(Date uptime) {
         this.uptime = uptime;
     }
 
+    /** Sets the date and time when the resource has created. */
     public UserComputationalResourceDTO withUptime(Date uptime) {
         setUptime(uptime);
         return this;
     }
 
+    /** Return the name of notebook shape where resource has been deployed. */
     public String getMasterShape() {
         return masterShape;
     }
 
+    /** Sets the name of notebook shape where resource has been deployed. */
     public void setMasterShape(String masterShape) {
         this.masterShape = masterShape;
     }
 
+    /** Sets the name of notebook shape where resource has been deployed. */
     public UserComputationalResourceDTO withMasterShape(String masterShape) {
         setMasterShape(masterShape);
         return this;
     }
 
+    // TODO: Comments
     public String getSlaveShape() {
         return slaveShape;
     }
 
+    // TODO: Comments
     public void setSlaveShape(String slaveShape) {
         this.slaveShape = slaveShape;
     }
 
+    // TODO: Comments
     public UserComputationalResourceDTO withSlaveShape(String slaveShape) {
         setSlaveShape(slaveShape);
         return this;
     }
 
+    // TODO: Comments
     public String getSlaveNumber() {
         return slaveNumber;
     }
 
+    // TODO: Comments
     public void setSlaveNumber(String slaveNumber) {
         this.slaveNumber = slaveNumber;
     }
 
+    // TODO: Comments
     public UserComputationalResourceDTO withSlaveNumber(String slaveNumber) {
         setSlaveNumber(slaveNumber);
         return this;
--- services/self-service/src/main/java/com/epam/dlab/backendapi/core/UserInstanceDTO.java
+++ services/self-service/src/main/java/com/epam/dlab/backendapi/core/UserInstanceDTO.java
@@ -25,6 +25,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/** Stores info about the user notebook.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserInstanceDTO {
     @JsonProperty("_id")
@@ -46,109 +48,134 @@ public class UserInstanceDTO {
     @JsonProperty("computational_resources")
     private List<UserComputationalResourceDTO> resources = new ArrayList<>();
 
+    /** Returns the unique id for the notebook. */
     public String getId() {
         return id;
     }
 
+    /** Returns the user login name. */
     public String getUser() {
         return user;
     }
 
+    /** Sets the user login name. */
     public void setUser(String user) {
         this.user = user;
     }
 
+    /** Sets the user login name. */
     public UserInstanceDTO withUser(String user) {
         setUser(user);
         return this;
     }
 
+    /** Returns the name of exploratory. */
     public String getExploratoryName() {
         return exploratoryName;
     }
 
+    /** Sets the name of exploratory. */
     public void setExploratoryName(String exploratoryName) {
         this.exploratoryName = exploratoryName;
     }
 
+    /** Sets the name of exploratory. */
     public UserInstanceDTO withExploratoryName(String exploratoryName) {
         setExploratoryName(exploratoryName);
         return this;
     }
 
+    /** Returns the exploratory id. */
     public String getExploratoryId() {
         return exploratoryId;
     }
 
+    /** Sets the exploratory id. */
     public void setExploratoryId(String exploratoryId) {
         this.exploratoryId = exploratoryId;
     }
 
+    /** Sets the exploratory id. */
     public UserInstanceDTO withExploratoryId(String exploratoryId) {
         setExploratoryId(exploratoryId);
         return this;
     }
 
+    /** Returns the status of notebook. */
     public String getStatus() {
         return status;
     }
 
+    /** Sets the status of notebook. */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /** Sets the status of notebook. */
     public UserInstanceDTO withStatus(String status) {
         setStatus(status);
         return this;
     }
 
+    /** Returns the name of notebook shape. */
     public String getShape() {
         return shape;
     }
 
+    /** Sets the name of notebook shape. */
     public void setShape(String shape) {
         this.shape = shape;
     }
 
+    /** Sets the name of notebook shape. */
     public UserInstanceDTO withShape(String shape) {
         setShape(shape);
         return this;
     }
 
+    /** Returns the URL of notebook. */
     public String getUrl() {
         return url;
     }
 
+    /** Sets the URL of notebook. */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /** Sets the URL of notebook. */
     public UserInstanceDTO withUrl(String url) {
         setUrl(url);
         return this;
     }
 
+    /** Returns the date and time when the notebook has created. */
     public Date getUptime() {
         return uptime;
     }
 
+    /** Sets the date and time when the notebook has created. */
     public void setUptime(Date uptime) {
         this.uptime = uptime;
     }
 
+    /** Sets the date and time when the notebook has created. */
     public UserInstanceDTO withUptime(Date uptime) {
         setUptime(uptime);
         return this;
     }
 
+    /** Returns a list of user's computational resources for notebook. */
     public List<UserComputationalResourceDTO> getResources() {
         return resources;
     }
 
+    /** Sets a list of user's computational resources for notebook. */
     public void setResources(List<UserComputationalResourceDTO> resources) {
         this.resources = resources;
     }
 
+    /** Sets a list of user's computational resources for notebook. */
     public UserInstanceDTO withResources(List<UserComputationalResourceDTO> resources) {
         setResources(resources);
         return this;
