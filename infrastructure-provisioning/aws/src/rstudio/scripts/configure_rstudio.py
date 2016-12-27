65d1b5664620bd7c0325b2a4d28e1ac65a861302
Vadym_Kuznetsov <Vadym_Kuznetsov@epam.com>
2016-12-27 13:33:05 +0200

    - fixed .Renviron file update when Rstudio notebook starts

--- infrastructure-provisioning/aws/src/rstudio/scripts/configure_rstudio.py
+++ infrastructure-provisioning/aws/src/rstudio/scripts/configure_rstudio.py
@@ -95,7 +95,9 @@ def install_rstudio():
             sudo('''echo 'library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))' >> /home/ubuntu/.Rprofile''')
             sudo('rstudio-server start')
             sudo('echo "ubuntu:' + args.rstudio_pass + '" | chpasswd')
+            sudo("sed -i '/exit 0/d' /etc/rc.local")
             sudo('''bash -c "echo \'sed -i 's/^#SPARK_HOME/SPARK_HOME/' /home/ubuntu/.Renviron\' >> /etc/rc.local"''')
+            sudo("bash -c 'echo exit 0 >> /etc/rc.local'")
             sudo('touch /home/ubuntu/.ensure_dir/rstudio_ensured')
         except:
             sys.exit(1)
