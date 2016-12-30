107d63e82194d2f69eeb30068388b1e324c09041
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2016-12-30 11:30:45 +0100

    removed useless path

--- services/self-service/src/main/resources/webapp/src/client/app/services/applicationServiceFacade.service.ts
+++ services/self-service/src/main/resources/webapp/src/client/app/services/applicationServiceFacade.service.ts
@@ -34,7 +34,6 @@ export class ApplicationServiceFacade {
   private static readonly COMPUTATIONAL_RESOURCES_TEMLATES = 'computational_resources_templates';
   private static readonly COMPUTATIONAL_RESOURCES = 'computational_resources';
   private static readonly USER_PREFERENCES = 'user_preferences';
-  private static readonly USER_PREFERENCES_UPDATE = 'user_preferences_update';
   private accessTokenKey: string = 'access_token';
   private requestRegistry: Dictionary<string>;
 
@@ -142,7 +141,7 @@ export class ApplicationServiceFacade {
 
   public buildUpdateUserPreferences(data): Observable<Response> {
     return this.buildRequest(RequestMethod.Post,
-      this.requestRegistry.Item(ApplicationServiceFacade.USER_PREFERENCES_UPDATE),
+      this.requestRegistry.Item(ApplicationServiceFacade.USER_PREFERENCES),
       data,
       this.getRequestOptions(true, true));
   }
@@ -174,7 +173,6 @@ export class ApplicationServiceFacade {
 
     // Filtering Configuration
     this.requestRegistry.Add(ApplicationServiceFacade.USER_PREFERENCES, '/api/user/settings');
-    this.requestRegistry.Add(ApplicationServiceFacade.USER_PREFERENCES_UPDATE, '/api/user/settings/save');
   }
 
   private buildRequest(method: RequestMethod, url: string, body: any, opt: RequestOptions): Observable<Response> {
