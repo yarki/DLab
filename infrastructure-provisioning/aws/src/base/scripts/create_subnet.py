91434f84b8b7c10c7b2370d2383142a52fbb1ae0
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-12-30 14:48:35 +0200

    debugging

--- infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
+++ infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
@@ -87,6 +87,7 @@ if __name__ == "__main__":
             route_table.associate_with_subnet(SubnetId=subnet_id)
         else:
             print "Subnet ID " + subnet_id
+            print "Path: " + os.environ['ssn_dlab_path']
             with open(os.environ['ssn_dlab_path'] + 'tmp/ssn_subnet_id', 'w') as f:
                 f.write(subnet_id)
             print "OS subnet ID " + os.environ['creds_subnet_id']
