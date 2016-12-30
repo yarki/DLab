4240829501a7d67a30b6e4c983a902e93f85d9b8
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2016-12-30 14:36:22 +0200

    debugging

--- infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
+++ infrastructure-provisioning/aws/src/base/scripts/create_subnet.py
@@ -88,6 +88,7 @@ if __name__ == "__main__":
         else:
             print "Subnet ID " + subnet_id
             ssn_subnet_id = subnet_id
+            print "Global " + ssn_subnet_id
             print "OS subnet ID " + os.environ['creds_subnet_id']
         success = True
     except:
