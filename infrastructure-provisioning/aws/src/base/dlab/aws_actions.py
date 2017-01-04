4922d6df1a7b998cbe11f17d4ffac3853510a118
Roman Glova <roman_glova@epam.com>
2017-01-03 19:34:51 -0500

    Added SPARK_HOME removing after emr stop

--- infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
+++ infrastructure-provisioning/aws/src/base/dlab/aws_actions.py
@@ -575,6 +575,10 @@ def remove_kernels(emr_name, tag_name, nb_tag_value, ssh_user, key_path, emr_ver
                 env.host_string = env.user + "@" + env.hosts
                 sudo('rm -rf  /opt/' + emr_version + '/' + emr_name + '/')
                 sudo('rm -rf /home/{}/.local/share/jupyter/kernels/*_{}'.format(ssh_user, emr_name))
+                if exists('/home/ubuntu/.ensure_dir/emr_interpreter_ensured'):
+                    sudo('sed -i \"s/^export SPARK_HOME.*/#export SPARK_HOME=/\" /opt/zeppelin/conf/zeppelin-env.sh')
+                    sudo("rm -rf /home/ubuntu/.ensure_dir/emr_interpreter_ensure")
+                    sudo("service zeppelin-notebook restart")
                 if exists('/home/ubuntu/.ensure_dir/rstudio_emr_ensured'):
                     sudo("sed -i '/" + emr_name + "/d' /home/ubuntu/.Renviron")
                     sudo("sed -i 's|/opt/" + emr_version + '/' + emr_name + "/spark//R/lib:||g' /home/ubuntu/.bashrc")
