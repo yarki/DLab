0519e25ab5f34aba831cbb7750f1e624ad526642
Usein Faradzhev <Usein_Faradzhev@epam.com>
2017-01-12 13:17:32 +0400

    Renamed kernel_test.py to kernels_test.py

--- automation/src/test/java/AutomationTest/TestServices.java
+++ automation/src/test/java/AutomationTest/TestServices.java
@@ -393,7 +393,7 @@ public class TestServices {
             throws JSchException, IOException, InterruptedException {
 
     	String [] files = {
-    			"kernel_test.py",
+    			"kernels_test.py",
     			"train.csv",
     			"PYTHON.ipynb",
     			"R.ipynb",
