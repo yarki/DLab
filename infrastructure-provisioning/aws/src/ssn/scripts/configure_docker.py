8276faeb7379bf217657de1da57686c7a17640b3
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-01-13 14:21:49 +0200

    fixed issue with host string

--- infrastructure-provisioning/aws/src/ssn/scripts/configure_docker.py
+++ infrastructure-provisioning/aws/src/ssn/scripts/configure_docker.py
@@ -37,7 +37,7 @@ def build_docker_images(image_list):
         sudo('mkdir /project_images; chown ' + os.environ['general_os_user'] + ' /project_images')
         print "KEYFILE: " + args.keyfile
         print "HOST_STRING: " + args.host_string
-        local('scp -r -i %s /project_tree/* %s:/project_images/' % (args.keyfile, args.host_string))
+        local('scp -r -i %s /project_tree/* %s:/project_images/' % (args.keyfile, env.host_string))
         for image in image_list:
             name = image['name']
             tag = image['tag']
