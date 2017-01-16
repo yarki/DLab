21e97c01da2d303f7a0c85a067edab629c051ec7
Vadym_Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-01-16 16:50:24 +0200

    - refactor rstudio

--- infrastructure-provisioning/src/rstudio/scripts/configure_rstudio.py
+++ infrastructure-provisioning/src/rstudio/scripts/configure_rstudio.py
@@ -30,6 +30,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--hostname', type=str, default='')
 parser.add_argument('--keyfile', type=str, default='')
 parser.add_argument('--region', type=str, default='')
+parser.add_argument('--os_user', type=str, default='')
 parser.add_argument('--rstudio_pass', type=str, default='')
 args = parser.parse_args()
 
@@ -42,19 +43,19 @@ templates_dir = '/root/templates/'
 
 
 def prepare_disk():
-    if not exists('/home/ubuntu/.ensure_dir/disk_ensured'):
+    if not exists('/home/' + args.os_user + '/.ensure_dir/disk_ensured'):
         try:
             sudo('''bash -c 'echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdb' ''')
             sudo('mkfs.ext4 /dev/xvdb1')
             sudo('mount /dev/xvdb1 /opt/')
             sudo(''' bash -c "echo '/dev/xvdb1 /opt/ ext4 errors=remount-ro 0 1' >> /etc/fstab" ''')
-            sudo('touch /home/ubuntu/.ensure_dir/disk_ensured')
+            sudo('touch /home/' + args.os_user + '/.ensure_dir/disk_ensured')
         except:
             sys.exit(1)
 
 
 def ensure_libraries_py():
-    if not exists('/home/ubuntu/.ensure_dir/ensure_libraries_py_installed'):
+    if not exists('/home/' + args.os_user + '/.ensure_dir/ensure_libraries_py_installed'):
         try:
             sudo('export LC_ALL=C')
             sudo('apt-get install python3-setuptools')
@@ -66,13 +67,13 @@ def ensure_libraries_py():
             sudo('pip3 install -U pip --no-cache-dir')
             sudo('pip3 install boto boto3 --no-cache-dir')
             sudo('pip3 install fabvenv fabric-virtualenv --no-cache-dir')
-            sudo('touch /home/ubuntu/.ensure_dir/ensure_libraries_py_installed')
+            sudo('touch /home/' + args.os_user + '/.ensure_dir/ensure_libraries_py_installed')
         except:
             sys.exit(1)
 
 
 def install_rstudio():
-    if not exists('/home/ubuntu/.ensure_dir/rstudio_ensured'):
+    if not exists('/home/' + args.os_user + '/.ensure_dir/rstudio_ensured'):
         try:
             sudo('apt-get install -y default-jre')
             sudo('apt-get install -y default-jdk')
@@ -86,42 +87,42 @@ def install_rstudio():
             sudo('wget https://download2.rstudio.org/rstudio-server-1.0.44-amd64.deb')
             sudo('gdebi -n rstudio-server-1.0.44-amd64.deb')
             sudo('mkdir /mnt/var')
-            sudo('chown ubuntu:ubuntu /mnt/var')
-            sudo('touch /home/ubuntu/.Renviron')
-            sudo('chown ubuntu:ubuntu /home/ubuntu/.Renviron')
-            sudo('''echo 'SPARK_HOME="''' + local_spark_path + '''"' >> /home/ubuntu/.Renviron''')
-            sudo('touch /home/ubuntu/.Rprofile')
-            sudo('chown ubuntu:ubuntu /home/ubuntu/.Rprofile')
-            sudo('''echo 'library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))' >> /home/ubuntu/.Rprofile''')
+            sudo('chown ' + args.os_user + ':' + args.os_user + ' /mnt/var')
+            sudo('touch /home/' + args.os_user + '/.Renviron')
+            sudo('chown ' + args.os_user + ':' + args.os_user + ' /home/' + args.os_user + '/.Renviron')
+            sudo('''echo 'SPARK_HOME="''' + local_spark_path + '''"' >> /home/''' + args.os_user + '''/.Renviron''')
+            sudo('touch /home/' + args.os_user + '/.Rprofile')
+            sudo('chown ' + args.os_user + ':' + args.os_user + ' /home/' + args.os_user + '/.Rprofile')
+            sudo('''echo 'library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))' >> /home/''' + args.os_user + '''/.Rprofile''')
             sudo('rstudio-server start')
-            sudo('echo "ubuntu:' + args.rstudio_pass + '" | chpasswd')
+            sudo('echo "' + args.os_user + ':' + args.rstudio_pass + '" | chpasswd')
             sudo("sed -i '/exit 0/d' /etc/rc.local")
-            sudo('''bash -c "echo \'sed -i 's/^#SPARK_HOME/SPARK_HOME/' /home/ubuntu/.Renviron\' >> /etc/rc.local"''')
+            sudo('''bash -c "echo \'sed -i 's/^#SPARK_HOME/SPARK_HOME/' /home/''' + args.os_user + '''/.Renviron\' >> /etc/rc.local"''')
             sudo("bash -c 'echo exit 0 >> /etc/rc.local'")
-            sudo('touch /home/ubuntu/.ensure_dir/rstudio_ensured')
+            sudo('touch /home/' + args.os_user + '/.ensure_dir/rstudio_ensured')
         except:
             sys.exit(1)
     else:
         try:
-            sudo('echo "ubuntu:' + args.rstudio_pass + '" | chpasswd')
+            sudo('echo "' + args.os_user + ':' + args.rstudio_pass + '" | chpasswd')
         except:
             sys.exit(1)
 
 
 def ensure_local_spark():
-    if not exists('/home/ubuntu/.ensure_dir/local_spark_ensured'):
+    if not exists('/home/' + args.os_user + '/.ensure_dir/local_spark_ensured'):
         try:
             sudo('wget ' + spark_link + ' -O /tmp/spark-' + spark_version + '-bin-hadoop' + hadoop_version + '.tgz')
             sudo('tar -zxvf /tmp/spark-' + spark_version + '-bin-hadoop' + hadoop_version + '.tgz -C /opt/')
             sudo('mv /opt/spark-' + spark_version + '-bin-hadoop' + hadoop_version + ' ' + local_spark_path)
-            sudo('chown -R ubuntu:ubuntu ' + local_spark_path)
-            sudo('touch /home/ubuntu/.ensure_dir/local_spark_ensured')
+            sudo('chown -R ' + args.os_user + ':' + args.os_user + ' ' + local_spark_path)
+            sudo('touch /home/' + args.os_user + '/.ensure_dir/local_spark_ensured')
         except:
             sys.exit(1)
 
 
 def ensure_s3_kernel():
-    if not exists('/home/ubuntu/.ensure_dir/s3_kernel_ensured'):
+    if not exists('/home/' + args.os_user + '/.ensure_dir/s3_kernel_ensured'):
         try:
             sudo('mkdir -p ' + s3_jars_dir)
             put(templates_dir + 'jars/local_jars.tar.gz', '/tmp/local_jars.tar.gz')
@@ -130,7 +131,7 @@ def ensure_s3_kernel():
             sudo("sed -i 's/URL/https:\/\/s3-{}.amazonaws.com/' /tmp/spark-defaults_local.conf".format(
                 args.region))
             sudo('\cp /tmp/spark-defaults_local.conf /opt/spark/conf/spark-defaults.conf')
-            sudo('touch /home/ubuntu/.ensure_dir/s3_kernel_ensured')
+            sudo('touch /home/' + args.os_user + '/.ensure_dir/s3_kernel_ensured')
         except:
             sys.exit(1)
 
@@ -142,12 +143,12 @@ if __name__ == "__main__":
     print "Configure connections"
     env['connection_attempts'] = 100
     env.key_filename = [args.keyfile]
-    env.host_string = 'ubuntu@' + args.hostname
+    env.host_string = args.os_user + '@' + args.hostname
 
     print "Configuring notebook server."
     try:
-        if not exists('/home/ubuntu/.ensure_dir'):
-            sudo('mkdir /home/ubuntu/.ensure_dir')
+        if not exists('/home/' + args.os_user + '/.ensure_dir'):
+            sudo('mkdir /home/' + args.os_user + '/.ensure_dir')
     except:
         sys.exit(1)
 
