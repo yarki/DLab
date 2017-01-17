3d878f3bbfdb5de084f3f0b190ca5a1deba654a6
volodymyr_shvachko <volodymyr_shvachko@epam.com>
2017-01-17 15:54:42 +0200

    Added couple of functions for RH

--- infrastructure-provisioning/src/general/lib/os/redhat/ssn_lib.py
+++ infrastructure-provisioning/src/general/lib/os/redhat/ssn_lib.py
@@ -17,3 +17,71 @@
 # limitations under the License.
 #
 # ******************************************************************************
+
+import crypt
+import yaml
+from dlab.fab import *
+from dlab.aws_meta import *
+import os
+
+def ensure_docker_daemon(dlab_path):
+    try:
+        if not exists('{}tmp/docker_daemon_ensured'.format(dlab_path)):
+            sudo('yum update -y')
+            sudo('curl -fsSL https://get.docker.com/ | sh')
+            sudo('systemctl enable docker.service')
+            sudo('systemctl start docker')
+            sudo('touch {}tmp/docker_daemon_ensured'.format(dlab_path))
+        return True
+    except:
+        return False
+
+
+def ensure_nginx(dlab_path):
+    try:
+        if not exists('{}tmp/nginx_ensured'.format(dlab_path)):
+            sudo('yum -y install nginx')
+            sudo('systemctl restart nginx.service')
+            sudo('chkconfig nginx on')
+            sudo('touch {}tmp/nginx_ensured'.format(dlab_path))
+        return True
+    except:
+        return False
+
+
+def ensure_jenkins(dlab_path):
+    try:
+        if not exists('{}tmp/jenkins_ensured'.format(dlab_path)):
+            sudo('wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo')
+            sudo('rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key')
+            sudo('yum -y install java')
+            sudo('yum -y install jenkins')
+            sudo('touch {}tmp/jenkins_ensured'.format(dlab_path))
+        return True
+    except:
+        return False
+
+
+def configure_jenkins(dlab_path, os_user):
+    try:
+        if not exists('{}tmp/jenkins_configured'.format(dlab_path)):
+            sudo('rm -rf /var/lib/jenkins/*')
+            sudo('mkdir -p /var/lib/jenkins/jobs/')
+            sudo('chown -R {0}:{0} /var/lib/jenkins/'.format(args.user))
+            put('/root/templates/jenkins_jobs/*', '/var/lib/jenkins/jobs/')
+            sudo("find /var/lib/jenkins/jobs/ -type f | xargs sed -i \'s/OS_USR/{}/g\'".format(os_user))
+            sudo('chown -R jenkins:jenkins /var/lib/jenkins')
+            sudo('/etc/init.d/jenkins stop; sleep 5')
+            sudo('sed -i \'/JENKINS_PORT/ s/^/#/\' /etc/sysconfig/jenkins; echo \'JENKINS_PORT="8070"\' >> /etc/sysconfig/jenkins')
+            sudo('sed -i \'/JENKINS_ARGS/ s|=""|="--prefix=/jenkins"|\' /etc/sysconfig/jenkins')
+            sudo('semanage port -a -t http_port_t -p tcp 8070')
+            sudo('setsebool -P httpd_can_network_connect 1')
+            sudo('chkconfig jenkins on')
+            sudo('systemctl start jenkins.service')
+            sudo('echo "jenkins ALL = NOPASSWD:ALL" >> /etc/sudoers')
+            sudo('touch {}tmp/jenkins_configured'.format(dlab_path))
+        return True
+    except:
+        return False
+
+
