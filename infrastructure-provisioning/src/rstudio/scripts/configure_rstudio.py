40389ef83bb978cce5bc620cdd0bd58ba186752c
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-01-18 20:52:12 +0200

    fix configure_rstudio (add arg to ensure_libraries_py)

--- infrastructure-provisioning/src/rstudio/scripts/configure_rstudio.py
+++ infrastructure-provisioning/src/rstudio/scripts/configure_rstudio.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     prepare_disk(args.os_user)
 
     print "Install python libraries"
-    ensure_libraries_py()
+    ensure_libraries_py(args.os_user)
 
     print "Install RStudio"
     install_rstudio(args.os_user, local_spark_path, args.rstudio_pass)
