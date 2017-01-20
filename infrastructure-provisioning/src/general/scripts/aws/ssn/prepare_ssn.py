1f9436b12eac8520f981b76b66c30f29d36ceff6
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-01-20 18:50:12 +0200

    fixed fabfiles

--- infrastructure-provisioning/src/general/scripts/aws/ssn/prepare_ssn.py
+++ infrastructure-provisioning/src/general/scripts/aws/ssn/prepare_ssn.py
@@ -241,7 +241,8 @@ if __name__ == "__main__":
         except Exception as err:
             append_result("Unable to create ssn instance: " + err)
             raise Exception
-    except:
+    except Exception as err:
+        append_result("E: " + err)
         remove_all_iam_resources(instance)
         remove_s3(instance)
         if pre_defined_sg:
