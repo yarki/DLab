8d588fa82c1d0040091053e8c9671f692a78ff1d
volodymyr_shvachko <volodymyr_shvachko@epam.com>
2017-02-01 17:06:34 +0200

    Changed method installing tensorflow. Finally working method

--- infrastructure-provisioning/src/general/lib/os/redhat/notebook_lib.py
+++ infrastructure-provisioning/src/general/lib/os/redhat/notebook_lib.py
@@ -258,6 +258,7 @@ def install_tensor(os_user, tensorflow_version, files_dir, templates_dir):
             sudo('mv cuda-repo-rhel7-8-0-local-8.0.44-1.x86_64-rpm cuda-repo-rhel7-8-0-local-8.0.44-1.x86_64.rpm; rpm -i cuda-repo-rhel7-8-0-local-8.0.44-1.x86_64.rpm')
             sudo('yum clean all')
             sudo('yum -y install cuda')
+            sudo('pip3 innstall --upgrade pip wheel numpy')
 
             sudo('mv /usr/local/cuda-8.0 /opt/')
             sudo('ln -s /opt/cuda-8.0 /usr/local/cuda-8.0')
@@ -274,7 +275,7 @@ def install_tensor(os_user, tensorflow_version, files_dir, templates_dir):
             sudo('wget https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-' + tensorflow_version + '-cp27-none-linux_x86_64.whl')
             sudo('wget https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-' + tensorflow_version + '-cp35-cp35m-linux_x86_64.whl')
             sudo('python2.7 -m pip install --upgrade tensorflow_gpu-' + tensorflow_version + '-cp27-none-linux_x86_64.whl')
-            run('python3 -m pip install --upgrade tensorflow_gpu-' + tensorflow_version + '-cp35-cp35m-linux_x86_64.whl')
+            run('python3.5 -m pip install --upgrade tensorflow_gpu-' + tensorflow_version + '-cp35-cp35m-linux_x86_64.whl')
             sudo('mkdir /var/log/tensorboard')
             put(templates_dir + 'tensorboard-python2.service', '/tmp/tensorboard-python2.service')
             put(templates_dir + 'tensorboard-python3.service', '/tmp/tensorboard-python3.service')
