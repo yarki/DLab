543942478b302312823fc01b1627c855ad7ad7da
Vadym_Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-02-07 18:31:58 +0200

    test change root volume size

--- infrastructure-provisioning/src/general/lib/aws/actions_lib.py
+++ infrastructure-provisioning/src/general/lib/aws/actions_lib.py
@@ -202,6 +202,13 @@ def create_instance(definitions, instance_tag):
             instances = ec2.create_instances(ImageId=definitions.ami_id, MinCount=1, MaxCount=1,
                                              BlockDeviceMappings=[
                                                  {
+                                                     "DeviceName": "/dev/sda1",
+                                                     "Ebs":
+                                                         {
+                                                             "VolumeSize": 12
+                                                         }
+                                                 },
+                                                 {
                                                      "DeviceName": "/dev/sdb",
                                                      "Ebs":
                                                          {
