0108b26392406066f18faf79af29470336fdfbce
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-02-16 22:46:54 +0200

    fixed issue with removing EMR kernel

--- infrastructure-provisioning/src/general/lib/aws/actions_lib.py
+++ infrastructure-provisioning/src/general/lib/aws/actions_lib.py
@@ -694,14 +694,16 @@ def remove_kernels(emr_name, tag_name, nb_tag_value, ssh_user, key_path, emr_ver
                 env.user = "{}".format(ssh_user)
                 env.key_filename = "{}".format(key_path)
                 env.host_string = env.user + "@" + env.hosts
-                livy_port = sudo("cat /opt/" + emr_version + "/" + emr_name
-                                 + "/livy/conf/livy.conf | grep livy.server.port | tail -n 1 | awk '{printf $3}'")
-                process_number = sudo("netstat -natp 2>/dev/null | grep ':" + livy_port +
-                                      "' | awk '{print $7}' | sed 's|/.*||g'")
-                sudo('kill -9 ' + process_number)
-                sudo('rm -rf  /opt/' + emr_version + '/' + emr_name + '/')
                 sudo('rm -rf /home/{}/.local/share/jupyter/kernels/*_{}'.format(ssh_user, emr_name))
                 if exists('/home/{}/.ensure_dir/emr_{}_interpreter_ensured'.format(ssh_user, emr_name)):
+                    try:
+                        livy_port = sudo("cat /opt/" + emr_version + "/" + emr_name
+                                         + "/livy/conf/livy.conf | grep livy.server.port | tail -n 1 | awk '{printf $3}'")
+                        process_number = sudo("netstat -natp 2>/dev/null | grep ':" + livy_port +
+                                              "' | awk '{print $7}' | sed 's|/.*||g'")
+                        sudo('kill -9 ' + process_number)
+                    except:
+                        print "Wasn't able to find Livy server for this EMR!"
                     sudo('sed -i \"s/^export SPARK_HOME.*/export SPARK_HOME=\/opt\/spark/\" /opt/zeppelin/conf/zeppelin-env.sh')
                     sudo("rm -rf /home/{}/.ensure_dir/emr_interpreter_ensure".format(ssh_user))
                     zeppelin_url = 'http://' + private + ':8080/api/interpreter/setting/'
@@ -726,6 +728,7 @@ def remove_kernels(emr_name, tag_name, nb_tag_value, ssh_user, key_path, emr_ver
                     if not sudo("sed -n '/^SPARK_HOME/p' /home/{}/.Renviron".format(ssh_user)):
                         sudo("sed -i '1!G;h;$!d;' /home/{0}/.Renviron; sed -i '1,3s/#//;1!G;h;$!d' /home/{0}/.Renviron".format(ssh_user))
                     sudo("sed -i 's|/opt/" + emr_version + '/' + emr_name + "/spark//R/lib:||g' /home/{}/.bashrc".format(ssh_user))
+                sudo('rm -rf  /opt/' + emr_version + '/' + emr_name + '/')
                 print "Notebook's " + env.hosts + " kernels were removed"
         else:
             print "There are no notebooks to clean kernels."
