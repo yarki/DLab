e184647c7ea176d1a4648036dc5b699444e4677c
Vadym Kuznetsov <Vadym_Kuznetsov@epam.com>
2017-02-18 00:44:03 +0200

    switch to Tensorflow 1.0.0 version both Ubuntu and Redhat

--- infrastructure-provisioning/src/general/lib/os/fab.py
+++ infrastructure-provisioning/src/general/lib/os/fab.py
@@ -183,7 +183,7 @@ def configure_jupyter(os_user, jupyter_conf_file, templates_dir):
             sudo("chmod 644 /tmp/jupyter-notebook.service")
             if os.environ['application'] == 'tensor':
                 sudo("sed -i 's|ExecStart=/bin/bash -c \"/usr/local/bin/jupyter notebook --config CONF_PATH\"|"
-                     "ExecStart=/bin/bash -c \"export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cudnn/lib64; "
+                     "ExecStart=/bin/bash -c \"export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cudnn/lib64:/usr/local/cuda/lib64; "
                      "/usr/local/bin/jupyter notebook --config CONF_PATH\"|' /tmp/jupyter-notebook.service")
             sudo("sed -i 's|CONF_PATH|" + jupyter_conf_file + "|' /tmp/jupyter-notebook.service")
             sudo("sed -i 's|OS_USR|" + os_user + "|' /tmp/jupyter-notebook.service")
