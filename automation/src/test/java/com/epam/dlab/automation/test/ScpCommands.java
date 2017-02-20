0551aadc544656d66ba3bc523735a0d00ea51a30
Nazar Barabash <Nazar.Barabash@cantire.com>
2017-02-20 12:25:56 +0200

    added lost file.

---
+++ automation/src/test/java/com/epam/dlab/automation/test/ScpCommands.java
@@ -0,0 +1,9 @@
+package com.epam.dlab.automation.test;
+
+
+public class ScpCommands {
+
+//    public static String copyToSSNCommand = "scp -i %s -o 'StrictHostKeyChecking no' %s ubuntu@%s:~/";
+    public static String copyToNotebookCommand = "scp -i %s -o 'StrictHostKeyChecking no' ~/%s %s@%s:/tmp/";
+    public static String runPythonCommand = "/usr/bin/python %s --region %s --bucket %s --cluster_name %s";
+}
