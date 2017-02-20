37d3acff5e99c9fa8e49b61015e04cd15046760e
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-02-20 17:34:26 +0200

    moved scala version to dlab ini file

--- infrastructure-provisioning/src/general/scripts/aws/jupyter_configure.py
+++ infrastructure-provisioning/src/general/scripts/aws/jupyter_configure.py
@@ -101,8 +101,10 @@ if __name__ == "__main__":
     try:
         logging.info('[CONFIGURE JUPYTER NOTEBOOK INSTANCE]')
         print '[CONFIGURE JUPYTER NOTEBOOK INSTANCE]'
-        params = "--hostname {} --keyfile {} --region {} --spark_version {} --hadoop_version {} --os_user {}".\
-            format(instance_hostname, keyfile_name, os.environ['aws_region'], os.environ['notebook_spark_version'], os.environ['notebook_hadoop_version'], os.environ['conf_os_user'])
+        params = "--hostname {} --keyfile {} --region {} --spark_version {} --hadoop_version {} --os_user {} --scala_version {}".\
+            format(instance_hostname, keyfile_name, os.environ['aws_region'], os.environ['notebook_spark_version'],
+                   os.environ['notebook_hadoop_version'], os.environ['conf_os_user'],
+                   os.environ['notebook_scala_version'])
         try:
             local("~/scripts/{}.py {}".format('configure_jupyter_node', params))
         except:
