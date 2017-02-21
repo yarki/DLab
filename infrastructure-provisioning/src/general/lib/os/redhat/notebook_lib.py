fc28cebd2eb826d9f30440aa660fb12006d087d9
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-02-21 16:48:22 +0200

    fixed issue with pip3

--- infrastructure-provisioning/src/general/lib/os/redhat/notebook_lib.py
+++ infrastructure-provisioning/src/general/lib/os/redhat/notebook_lib.py
@@ -281,7 +281,7 @@ def install_maven():
 
 def install_livy_dependencies():
     sudo('pip install cloudpickle requests requests-kerberos flake8 flaky pytest')
-    sudo('pip3 install cloudpickle requests requests-kerberos flake8 flaky pytest')
+    sudo('pip3.5 install cloudpickle requests requests-kerberos flake8 flaky pytest')
 
 
 def install_maven_emr():
@@ -292,4 +292,4 @@ def install_maven_emr():
 
 def install_livy_dependencies_emr():
     local('sudo pip install cloudpickle requests requests-kerberos flake8 flaky pytest')
-    local('sudo pip3 install cloudpickle requests requests-kerberos flake8 flaky pytest')
+    local('sudo pip3.5 install cloudpickle requests requests-kerberos flake8 flaky pytest')
