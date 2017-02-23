cc6d7552907ac4b7c71331a8cc60bc0ba376f88f
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-02-23 19:19:38 +0200

    fixed issue with variables

--- infrastructure-provisioning/src/general/scripts/aws/common_prepare_hadoop_cluster.py
+++ infrastructure-provisioning/src/general/scripts/aws/common_prepare_hadoop_cluster.py
@@ -84,9 +84,9 @@ if __name__ == "__main__":
     tag = {"Key": "{}-Tag".format(emr_conf['service_base_name']), "Value": "{}-{}-subnet".format(emr_conf['service_base_name'], os.environ['edge_user_name'])}
     emr_conf['subnet_cidr'] = get_subnet_by_tag(tag)
     emr_conf['key_path'] = os.environ['conf_key_dir'] + '/' + os.environ['conf_key_name'] + '.pem'
-    if bool(os.environ['slave_instance_spot']):
+    if bool(os.environ['emr_slave_instance_spot']):
         emr_conf['slave_bid_price'] = (float(get_ec2_price(emr_conf['slave_instance_type'], emr_conf['region'])) *
-                                       int(os.environ['slave_instance_spot_pct_price']))/100
+                                       int(os.environ['emr_slave_instance_spot_pct_price']))/100
     else:
         emr_conf['slave_bid_price'] = 0
 
