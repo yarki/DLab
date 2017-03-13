4aaf654384c3a7f637e0047ba8e58b64144c697f
Oleh Martushevskyi <Oleh_Martushevskyi@epam.com>
2017-03-13 14:16:49 +0200

    [EPMCBDCCSS-1161]: corrected bash script

--- infrastructure-provisioning/scripts/deploy_dlab.sh
+++ infrastructure-provisioning/scripts/deploy_dlab.sh
@@ -5,7 +5,7 @@
 #Print usage statement if incorrect number of command line args
 
 if [ "$#" -lt 5 ]; then
-  echo "Usage: ./deploy_dlab.sh <INFRASTRUCTURE_TAG> <ACCESS_KEY_ID> <SECRET_ACCESS_KEY> <AWS_REGION> <ACTION>"
+  echo "Usage: ./deploy_dlab.sh <INFRASTRUCTURE_TAG> <ACCESS_KEY_ID> <SECRET_ACCESS_KEY> <AWS_REGION> <OS_FAMILY> <CLOUD_PROVIDER> <OS_USER> <ACTION>"
   exit 1
 fi
 
@@ -13,7 +13,10 @@ Infrastructure_Tag=$1
 Access_Key_ID=$2
 Secret_Access_Key=$3
 Region=$4
-Action=$5
+OS_family=$5
+Cloud_provider=$6
+OS_user=$7
+Action=$8
 
 if [ -z "${WORKSPACE+x}" ]; then WORKSPACE="$PWD"; echo "WORKSPACE is not set, using current dir: $WORKSPACE"; else echo "Using workspace path from environment variable: $WORKSPACE"; fi
 
@@ -34,9 +37,9 @@ function buildServices(){
 
 function buildDockers(){
   # Build base and ssn docker
-  cd infrastructure-provisioning/aws/src || exit 1
-  sudo docker build --file base/Dockerfile -t docker.dlab-base base/
-  sudo docker build --file ssn/Dockerfile -t docker.dlab-ssn .
+  cd infrastructure-provisioning/src || exit 1
+  sudo docker build --build-arg OS=$OS_family --build-arg CLOUD=$Cloud_provider --file base/Dockerfile -t docker.dlab-base base/
+  sudo docker build --build-arg OS=$OS_family --build-arg CLOUD=$Cloud_provider --file ssn/Dockerfile -t docker.dlab-ssn .
 }
 
 function deployDlab(){
@@ -54,7 +57,8 @@ function deployDlab(){
 
   # Create SSN node and deploy DLab
   sudo docker run -i -v /root/BDCC-DSS-POC.pem:/root/keys/BDCC-DSS-POC.pem \
-    -v "$WORKSPACE"/web_app:/root/web_app -e "resource=ssn" -e "aws_ssn_instance_size=t2.medium" \
+    -v "$WORKSPACE"/web_app:/root/web_app -e "conf_os_family=$OS_family" -e "conf_os_user=$OS_user" \
+        -e "conf_cloud_provider=$Cloud_provider" -e "resource=ssn" -e "aws_ssn_instance_size=t2.medium" \
         -e "aws_region=us-west-2" -e "aws_vpc_id=vpc-83c469e4" -e "aws_subnet_id=subnet-22db937a" \
         -e "aws_security_groups_ids=sg-4d42dc35,sg-f19a0389,sg-71e27b09,sg-d3e67fab" -e "conf_key_name=BDCC-DSS-POC" \
         -e "conf_service_base_name=$Infrastructure_Tag" \
